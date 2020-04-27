b399cde1c6a7ff1e99db67fe275f7b4c03f09472
lucianohgo <luciano.hg4@gmail.com>
2020-04-27 19:27:12 -0300

    Template Initial Commit
    - Starts from Next.JS/learn template
    - Followed the tutorial to learn about the Building blocks of Next.JS

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css';
+import Layout from '../../components/Layout'
+import Date from '../../components/date'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData
+    }
+  }
+}
