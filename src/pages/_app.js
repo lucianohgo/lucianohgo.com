46abe0edbd580a8a61f3739427e5a409f77dbf89
Luciano H. Gomes <luciano.hg4@gmail.com>
2020-05-07 07:38:02 -0300

    Adds Core Web Vitals Tracking (#2)
    * Adds Core Web Vitals Tracking
    
    - Importing dynamically to avoid the import on the server side
    
    * Removing unecessary memoization

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,17 @@
+import { useEffect } from 'react';
 import Router from 'next/router';
-import { pageview } from '../lib/gtag';
-
+import { pageview } from '../lib/tracking';
 import '../styles/global.css';
 import '../styles/solarized-dark.theme.css';
 import '../fonts/fonts.css';
+import { setUpCoreVitalsTracking } from '../lib/performance-metrics';
 
 Router.events.on('routeChangeComplete', (url) => pageview(url));
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    setUpCoreVitalsTracking();
+  }, []);
+
   return <Component {...pageProps} />;
 }
