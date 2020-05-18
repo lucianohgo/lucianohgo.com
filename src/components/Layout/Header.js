ab5404ea6d13e8712876df51de7f15defecf9135
lucianohgo <luciano.hg4@gmail.com>
2020-05-18 11:01:13 -0300

    Adds Dark Mode

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import styles from './layout.module.css';
 import utilStyles from '../../styles/utils.module.css';
 import Link from 'next/link';
+import { ThemeToggle } from '../ThemeToggle';
 
 const Heading = ({ home, children }) =>
   home ? <h1>{children}</h1> : <h3>{children}</h3>;
@@ -8,12 +9,19 @@ const Heading = ({ home, children }) =>
 export default function Header({ home, name }) {
   return (
     <header className={styles.header}>
-      <nav className={`${styles.navHomeLink} ${utilStyles.headingMd}`}>
+      <nav
+        className={`${styles.nav} ${styles.navHomeLink} ${utilStyles.headingMd}`}
+      >
         <Heading home={home}>
           <Link href="/">
             <a>{name}</a>
           </Link>
         </Heading>
+        <ul className={styles.navLinks}>
+          <li>
+            <ThemeToggle />
+          </li>
+        </ul>
       </nav>
     </header>
   );
